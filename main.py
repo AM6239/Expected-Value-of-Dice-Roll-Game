891571e1f6c2e3ed1c9caa4fe7f3c6534d0da7f4
AM6239 <alumnae.psyche0c@icloud.com>
2024-12-24 00:30:50 +0000

    Test SSH signed commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# A simple script to estimate the expected value of taking the max of a three six-sided dice roll, and plot a histogram of the results.
+# A simple script to estimate the expected value of taking the max of a three six-sided dice roll, plotting a histogram of the results.
 
 import random
 import matplotlib.pyplot as plt
